e38f3e2621a1c02b1710e95d5b545b84985951bd
BertieCanCodeMaybe <153751539+BertieCanCodeMaybe@users.noreply.github.com>
2025-08-30 17:15:00 +0100

    Add decsriptors so the iphone app picks it up as a 384kz mic

--- examples/usb_microphone/usb_descriptors.c
+++ examples/usb_microphone/usb_descriptors.c
@@ -114,11 +114,20 @@ uint8_t const * tud_descriptor_configuration_cb(uint8_t index)
 // array of pointer to string descriptors
 char const* string_desc_arr [] =
 {
-    (const char[]) { 0x09, 0x04 }, 	// 0: is supported language is English (0x0409)
-    "PaniRCorp",                   	// 1: Manufacturer
-    "MicNode",              		// 2: Product
-    "123456",                      	// 3: Serials, should use chip ID
-    "UAC2",                 	 	// 4: Audio Interface
+//libby copied from pippistrelle
+
+    (const char[]) { 0x09, 0x04 },         // 0: is supported language is English (0x0409)
+    "DIY",                             // 1: Manufacturer
+    "Bat Detector 384kHz microphone",  // 2: Product
+    "666",                                 // 3: Serials, should use chip ID
+    "UAC2",                                // 4: Audio Interface
+
+
+//    (const char[]) { 0x09, 0x04 }, 	// 0: is supported language is English (0x0409)
+//    "PaniRCorp",                   	// 1: Manufacturer
+//    "MicNode",              		// 2: Product
+//    "123456",                      	// 3: Serials, should use chip ID
+//    "UAC2",                 	 	// 4: Audio Interface
 };
 
 static uint16_t _desc_str[32];
